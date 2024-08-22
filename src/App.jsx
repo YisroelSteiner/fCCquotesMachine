e2c8398cd0f9ae48224749e68c22a6f7555e4d0f
Yisroel Steiner <yisroeler@gmail.com>
2024-08-22 13:50:52 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+
+
+export default class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.generateQuote();
+        this.state = {
+          quote: "",
+          author: ""
+        };
+        this.generateQuote = this.generateQuote.bind(this);
+      }
+      
+      generateQuote() {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      axios.get("https://api.quotable.io/random", config).then((res)=>{
+        console.log(res.data)
+        this.setState({
+          quote: res.data.content
+        });
+        this.setState({ 
+          author: res.data.author
+        });
+      }) 
+    }
+   
+    render() {
+       
+        return  (<div id="quote-box" className="container">
+            <h3>Quotes Machine</h3>
+            <div id="text" className="quote">{this.state.quote}</div>
+            <p id="author">{this.state.author}</p>
+            <button id="new-quote" className="btn" onClick={this.generateQuote}>New quote</button>
+            <a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_blank" className="btn" >Tweet this wisdom</a>
+        </div>);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App.jsx';
+import './styles/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
